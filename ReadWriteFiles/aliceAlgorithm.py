6d0a3da43aa919b8e61f14fc86a43d47951c21c2
ProfBrauda <pamela.brauda@fscj.edu>
2023-02-24 14:26:54 -0500

    files for the Files lesson

---
+++ ReadWriteFiles/aliceAlgorithm.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# alice.py
+# analyze Alice in Wonderland text
+# from Matthes Crash Course 2e
+
+## Create a CONSTANT for your file name
+
+
+## Use try to open the file (with 'utf-8' encoding)
+
+
+##    Use the read method to read the file into a variable
+
+
+## catch the FileNotFoundError, printing a message to the use
+##    "Sorry, the file <filename> does not exist."
+##    Use the CONSTANT to display the actual filename
+
+
+
+## Use an else clause to split the file into words
+##    Use the len function to count the number of words
+##    Print a message with the name of the file and the number of words.
+
+
